003097da96ae154c0203e135a0d6bf8204742c4d
Rahulraj <rahulrohiniraju@gmail.com>
2023-12-09 08:01:32 +0530

    news detail render error solved

--- lib/screens/news/news_detail.dart
+++ lib/screens/news/news_detail.dart
@@ -36,42 +36,43 @@ class NewsDetail extends StatelessWidget {
               ),
               Expanded(
                 child: Container(
+                  height: double.infinity,
+                  width: double.infinity,
                   margin:
                       const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
                   decoration: BoxDecoration(
                       color: Colors.white.withAlpha(180),
                       borderRadius: BorderRadius.circular(15)),
-                  child: Expanded(
-                    child: ListView(
-                      children: [
-                        Image(image: NetworkImage(cover)).p8,
-                        Text(date).p8,
-                        Text(
-                          title,
-                          style: const TextStyle(
-                              fontSize: 22, fontWeight: FontWeight.bold),
-                        ).p8,
-                        Container(
-                          height: 80,
-                          width: double.infinity,
-                          color: Colors.red,
-                          alignment: Alignment.center,
-                          child: const Text("A D   W I D G E T"),
-                        ),
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      Image(image: NetworkImage(cover)).p8,
+                      Text(date).p8,
+                      Text(
+                        title,
+                        style: const TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold),
+                      ).p8,
+                      Container(
+                        height: 80,
+                        width: double.infinity,
+                        color: Colors.red,
+                        alignment: Alignment.center,
+                        child: const Text("A D   W I D G E T"),
+                      ),
 
-                        Html(
-                          data: description,
-                          style: {
-                            "p": Style(
-                                fontSize: FontSize.medium,
-                                textAlign: TextAlign.justify),
-                          },
-                        )
-                        // Text(NewsManager().parseHtmlString(description) ??
-                        // "Loading...")
-                      ],
-                    ).p16,
-                  ),
+                      Html(
+                        data: description,
+                        style: {
+                          "p": Style(
+                              fontSize: FontSize.medium,
+                              textAlign: TextAlign.justify),
+                        },
+                      )
+                      // Text(NewsManager().parseHtmlString(description) ??
+                      // "Loading...")
+                    ],
+                  ).p16,
                 ),
               ),
             ],
